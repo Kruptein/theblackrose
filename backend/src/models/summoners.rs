da3cfbc40a30b5d38b6fb05eff490957b1d7a3a9
Darragh Van Tichelen <info@darragh.dev>
2020-11-09 12:35:16 +0100

    Add Summoner model

---
+++ backend/src/models/summoners.rs
@@ -0,0 +1,25 @@
+use crate::schema::*;
+
+#[derive(Debug, Deserialize, Queryable, Serialize)]
+pub struct Summoner {
+    pub id: i32,
+    pub account_id: String,
+    pub profile_icon_id: i32,
+    pub revision_date: i64,
+    pub name: String,
+    pub summoner_id: String,
+    pub puuid: String,
+    pub summoner_level: i64,
+}
+
+#[derive(Insertable, Debug)]
+#[table_name = "summoners"]
+pub struct NewSummoner<'a> {
+    pub account_id: &'a str,
+    pub profile_icon_id: i32,
+    pub revision_date: i64,
+    pub name: &'a str,
+    pub summoner_id: &'a str,
+    pub puuid: &'a str,
+    pub summoner_level: i64,
+}
--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -1,4 +1,17 @@
 table! {
+    summoners (id) {
+        id -> Int4,
+        account_id -> Text,
+        profile_icon_id -> Int4,
+        revision_date -> Int8,
+        name -> Text,
+        summoner_id -> Text,
+        puuid -> Text,
+        summoner_level -> Int8,
+    }
+}
+
+table! {
     users (id) {
         id -> Int4,
         auth0_subject -> Text,
@@ -6,3 +19,11 @@ table! {
         created_at -> Timestamp,
     }
 }
+
+joinable!(connections -> summoners (summoner_id));
+joinable!(connections -> users (user_id));
+
+allow_tables_to_appear_in_same_query!(
+    summoners,
+    users,
+);
