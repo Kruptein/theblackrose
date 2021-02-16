f25e76145ae2bfcea20dfc45c6b9a72f6df13de0
Darragh Van Tichelen <info@darragh.dev>
2021-02-16 19:21:55 +0100

    Remove unused InputUser

--- backend/src/models/users.rs
+++ backend/src/models/users.rs
@@ -1,13 +1,7 @@
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(sqlx::Type)]
 pub struct User {
     pub id: i32,
     pub auth0_subject: String,
     pub email: String,
     pub created_at: chrono::NaiveDateTime,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct InputUser {
-    pub auth0_subject: String,
-    pub email: String,
 }
