2783bd14ba9adca7adcc5db8e32b1965b8ae954c
Darragh Van Tichelen <info@darragh.dev>
2021-05-22 14:38:56 +0200

    Fix Records not needing suspense

--- frontend/src/views/Records.vue
+++ frontend/src/views/Records.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent, onMounted, ref } from "vue";
+import { defineComponent, onMounted, ref, watchEffect } from "vue";
 
 import { backendUrl, getAuthHeader } from "../api/utils";
 import MatchList from "../components/MatchList.vue";
@@ -17,6 +17,10 @@ export default defineComponent({
         const loading = ref(true);
         const records = ref<Record[]>([]);
         const matches = ref<MatchFeedElement[]>([]);
+
+        watchEffect(() => {
+            console.log(matches.value);
+        });
 
         onMounted(async () => {
             const headers = await getAuthHeader();
@@ -39,34 +43,22 @@ export default defineComponent({
             src="https://vignette3.wikia.nocookie.net/leagueoflegends/images/6/6c/Black_Rose.png"
         />
 
-        <!-- <template v-if="loading">
+        <template v-if="loading">
             <h1>Waiting for server data</h1>
         </template>
         <template v-else>
-            <div v-for="record in records" class="record-title" :key="record.recordType">
-                <span class="font-semi-bold">{{ RecordType[record.recordType] }}</span>
-                in {{ getQueueFromId(record.queueId) }} by {{ record.name }}:
-                <span class="font-semi-bold">{{ record.value }}</span>
-            </div>
-        </template> -->
-        <Suspense>
-            <template #default>
-                <div style="display: contents">
-                    <h1>Records</h1>
-                    <div v-for="(record, i) in records" class="record-title" :key="record.id">
-                        <span class="font-semi-bold">{{ RecordType[record.recordType] }}</span>
-                        in {{ getQueueFromId(record.queueId) }} by {{ record.name }}
-                        :
-                        <span class="font-semi-bold">{{ record.value }}</span>
-                        <span class="gameId" style="font-size: xx-small">[{{ record.gameId }}]</span>
-                        <MatchList :match-data="[matches[i]]" :visible-names="[record.name]" />
-                    </div>
+            <div style="display: contents">
+                <h1>Records</h1>
+                <div v-for="(record, i) in records" class="record-title" :key="record.id">
+                    <span class="font-semi-bold">{{ RecordType[record.recordType] }}</span>
+                    in {{ getQueueFromId(record.queueId) }} by {{ record.name }}
+                    :
+                    <span class="font-semi-bold">{{ record.value }}</span>
+                    <span class="gameId" style="font-size: xx-small">[{{ record.gameId }}]</span>
+                    <MatchList :match-data="[matches[i]]" :visible-names="[record.name]" />
                 </div>
-            </template>
-            <template #fallback>
-                <h1>Waiting for server data</h1>
-            </template>
-        </Suspense>
+            </div>
+        </template>
     </div>
 </template>
 
