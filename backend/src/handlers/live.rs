ed98c6f4bd1e30a2cd8252a3a7916efd7dbca37e
Darragh Van Tichelen <info@darragh.dev>
2022-05-15 19:15:56 +0200

    Update live data view

--- backend/src/handlers/live.rs
+++ backend/src/handlers/live.rs
@@ -1,4 +1,7 @@
-use riven::{consts::Champion, models::spectator_v4::CurrentGameInfo};
+use riven::{
+    consts::{Champion, Team},
+    models::spectator_v4::CurrentGameInfo,
+};
 use sqlx::{Error, PgPool};
 
 use crate::db::stats::get_winrate_for_champion;
@@ -7,6 +10,7 @@ use crate::db::stats::get_winrate_for_champion;
 pub struct ParticipantLiveInfo {
     summoner: String,
     champion: Champion,
+    team: Team,
     wins: u16,
     total: u16,
 }
@@ -27,6 +31,7 @@ pub async fn get_live_info(
         participant_info.push(ParticipantLiveInfo {
             summoner: participant.summoner_name,
             champion: participant.champion_id,
+            team: participant.team_id,
             wins: winrate.wins,
             total: winrate.total,
         })
